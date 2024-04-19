2fff44a283fc082a368275cbd05ed6551474e39a
ABDULLAH AL SAIF <121724625+alsaifdev@users.noreply.github.com>
2024-04-20 02:09:24 +0600

    Update toast.js

--- toast.js
+++ toast.js
@@ -1,214 +1,279 @@
-let link = document.createElement('link');
-link.setAttribute('href', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css');
-link.setAttribute('rel', 'stylesheet');
-let style = document.createElement('style');
-style.innerHTML = `
+let icons = {
+      success: `
+       <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 8.207-4 4a1 1 0 0 1-1.414 0l-2-2a1 1 0 0 1 1.414-1.414L9 10.586l3.293-3.293a1 1 0 0 1 1.414 1.414Z" /></svg>`,
+      info: `
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 20"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.147 15.085a7.159 7.159 0 0 1-6.189 3.307A6.713 6.713 0 0 1 3.1 15.444c-2.679-4.513.287-8.737.888-9.548A4.373 4.373 0 0 0 5 1.608c1.287.953 6.445 3.218 5.537 10.5 1.5-1.122 2.706-3.01 2.853-6.14 1.433 1.049 3.993 5.395 1.757 9.117Z" /></svg>`,
+      warning: `
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM10 15a1 1 0 1 1 0-2 1 1 0 0 1 0 2Zm1-4a1 1 0 0 1-2 0V6a1 1 0 0 1 2 0v5Z" /></svg>`,
+      danger: `
+      <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20"><path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5Zm3.707 11.793a1 1 0 1 1-1.414 1.414L10 11.414l-2.293 2.293a1 1 0 0 1-1.414-1.414L8.586 10 6.293 7.707a1 1 0 0 1 1.414-1.414L10 8.586l2.293-2.293a1 1 0 0 1 1.414 1.414L11.414 10l2.293 2.293Z" /></svg>`,
+      error: `
+      <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg"><path d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z" /></svg>
+      `,
+      loading: `
+      <div class="w-5 h-5 spinner"></div>
+      `,
+      close: `
+      <svg class="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6" /></svg>
+      `
+    }
+    let style = document.createElement('style');
+    style.innerHTML = `
     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&display=swap');
 
-    body {
-      font-family: 'Poppins', sans-serif;
-      margin: 0;
-      padding:0;
-      box-sizing:border-box;
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
     }
 
-    button {
-      padding: 10px;
+    .close-btn {
+    padding: 10px;
+    border: none;
+    background: white;
     }
 
     .top-right {
-      position: fixed;
-      top: 20px;
-      right: 5px;
+    position: fixed;
+    top: 20px;
+    right: 10px;
     }
+
     .top-center {
-      position: fixed;
-      top: 20px;
-      left: 50%;
-      transform: translateX(-50%);
+    position: fixed;
+    top: 20px;
+    left: 50%;
+    transform: translateX(-50%);
     }
-   .top-left {
-      position: fixed;
-      top: 20px;
-      left: 5px;
+
+    .top-left {
+    position: fixed;
+    top: 20px;
+    left: 10px;
     }
-        .bottom-right {
-            position: fixed;
-            bottom: 20px;
-            right: 5px;
-          }
-          .bottom-center {
-            position: fixed;
-            bottom: 20px;
-            left: 50% ;
-            transform: translateX(-50%);
-          }
-          .bottom-left {
-            position: fixed;
-            bottom: 20px;
-            left: 5px;
-          }
-        .center-right {
-            position: fixed;
-            top: 50%;
-            right: 5px;
-            transform: translateY(-50%);
-          }
-          .center-center {
-            position: fixed;
-            top: 50%;
-            left: 50% ;
-            transform: translate(-50%,-50%);
-          }
-          .center-left {
-            position: fixed;
-            top: 50%;
-            left: 5px;
-            transform: translateY(-50%);
-          }
-    
+
+    .bottom-right {
+    position: fixed;
+    bottom: 20px;
+    right: 10px;
+    }
+
+    .bottom-center {
+    position: fixed;
+    bottom: 20px;
+    left: 50%;
+    transform: translateX(-50%);
+    }
+
+    .bottom-left {
+    position: fixed;
+    bottom: 20px;
+    left: 10px;
+    }
+
+    .center-right {
+    position: fixed;
+    top: 50%;
+    right: 10px;
+    transform: translateY(-50%);
+    }
+
+    .center-center {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    }
+
+    .center-left {
+    position: fixed;
+    top: 50%;
+    left: 10px;
+    transform: translateY(-50%);
+    }
+
 
     .toast {
-      position: relative;
-      padding: 10px;
-      color: #fff;
-      margin-bottom: 10px;
-      width: 400px;
-      display: grid;
-      grid-template-columns: 70px 1fr 70px;
-      border-radius: 5px;
-      --color: #0abf30;
-      background-image:
-        linear-gradient(to right, #0abf3055, #22242f 30%);
-      animation: show 0.3s ease 1 forwards
+    font-family: 'Poppins', sans-serif;
+    position: relative;
+    display: flex;
+    padding: 0.5rem;
+    margin-bottom: 1rem;
+    align-items: center;
+    border-radius: 0.5rem;
+    width: 100%;
+    max-width: 20rem;
+    color: #6B7280;
+    background-color: #ffffff;
+    box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
     }
 
-    .toast i {
-      color: var(--color);
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      font-size: x-large;
+    .toast .icon-style {
+    display: inline-flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 0.5rem;
+    width: 2rem;
+    height: 2rem;
+
     }
 
     .toast .title {
-      font-size: 15px;
-      font-weight: bold;
+    margin-left: 0.75rem;
+    font-size: 0.875rem;
+    font-weight: 400;
     }
 
-    .toast span,
-    .toast i:nth-child(3) {
-      color: #fff;
-      opacity: 0.6;
-    }
-
-    @keyframes show {
-      0% {
-        transform: translateX(100%);
-      }
-
-      40% {
-        transform: translateX(-5%);
-      }
-
-      80% {
-        transform: translateX(0%);
-      }
-
-      100% {
-        transform: translateX(-0%);
-      }
-    }
-
-    .toast.progress::before {
-      position: absolute;
-      bottom: 0;
-      left: 0;
-      background-color: var(--color);
-      width: 100%;
-      height: 3px;
-      content: '';
-      box-shadow: 0 0 10px var(--color);
-      animation: timeOut var(--duration) linear 1 forwards
+    .progressBar {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    border-bottom-left-radius: 0.375rem;
+    width: 100%;
+    height: 0.25rem;
+    animation: timeOut var(--duration) linear 1 forwards
     }
 
     @keyframes timeOut {
+    to {
+    width: 0;
+    }
+    }
+
+    .icon-style.success {
+    color: #10B981;
+    background-color: #D1FAE5;
+    }
+
+    .icon-style.error {
+    color: #EF4444;
+    background-color: #FEE2E2;
+    }
+    .icon-style.danger {
+    color: #EF4444;
+    background-color: #FEE2E2;
+    }
+    .icon-style.warning {
+    color: rgb(249 115 22);
+    background-color: rgb(255 237 213);
+    }
+
+    .icon-style.info {
+    color: #3B82F6;
+    background-color: #DBEAFE;
+    }
+    .spinner {
+    border: 4px solid #C5CAFF;
+    border-top-color: #5C69F4;
+    border-radius: 50%;
+    animation: spin 1s linear infinite;
+    }
+
+    @keyframes spin {
+      from {
+         transform: rotate(0deg);
+      }
+
       to {
-        width: 0;
+         transform: rotate(360deg);
       }
     }
 
-    .toast.error {
-      --color: #f24d4c;
-      background-image:
-        linear-gradient(to right, #f24d4c55, #22242F 30%);
+    .progressBar.success {
+    background-color: #10B981;
     }
 
-    .toast.warning {
-      --color: #e9bd0c;
-      background-image:
-        linear-gradient(to right, #e9bd0c55, #22242F 30%);
+    .progressBar.error {
+    background-color: #EF4444;
+    }
+    .progressBar.danger {
+    background-color: #EF4444;
     }
 
-    .toast.info {
-      --color: #3498db;
-      background-image:
-        linear-gradient(to right, #3498db55, #22242F 30%);
+    .progressBar.warning {
+    background-color: rgb(249 115 22);
     }
-  `;
-document.head.appendChild(link)
-document.head.appendChild(style)
-class Toast {
-  constructor(option = {}) {
-    this.notifications = document.createElement('div');
-    this.notifications.classList.add(option.position ? option.position : 'top-right')
-    this.notifications.setAttribute('style',`--duration:${(option.autoClose ? option.autoClose : 5000) / 1000}s`)
-    document.body.appendChild(this.notifications)
-    this.createToast = (type, icon, title, text) => {
-      let newToast = document.createElement('div');
-      newToast.innerHTML = `
-            <div class="toast ${option.progress && 'progress'} ${type}">
-              <i class="${icon}"></i>
-              <div class="content">
-                <div class="title">${title}</div>
-                  <span>${text}</span>
-                </div>
-              ${option.closeOnClick ? ` <i class="fa-solid fa-xmark" onclick="(this.parentElement).remove()"></i> ` : ''}
-            </div>`;
-      this.notifications.appendChild(newToast)
-      newToast.timeOut = setTimeout(
-        () => newToast.remove(), option.autoClose ? option.autoClose : 5000
-      )
+
+    .progressBar.info {
+    background-color: #3B82F6;
     }
-  }
-  success(title = 'Success', option = {}) {
-    let type = 'success';
-    let icon = option.icon ? option.icon : 'fa-solid fa-circle-check';
-    let text = option.discription ? option.discription : 'This is a Success toast.';
-    this.createToast(type, icon, title, text);
-  }
-  error(title = 'Error', option = {}) {
-    let type = 'error';
-    let icon = option.icon ? option.icon : 'fa-solid fa-circle-exclamation';
-    let text = option.discription ? option.discription : 'This is a error toast.';
-    this.createToast(type, icon, title, text);
-  }
-  warning(title = 'Warning', option = {}) {
-    let type = 'warning';
-    let icon = option.icon ? option.icon : 'fa-solid fa-triangle-exclamation';
-    let text = option.discription ? option.discription : 'This is a Warning toast.';
-    this.createToast(type, icon, title, text);
-  }
-  info(title = 'Info', option = {}) {
-    let type = 'info';
-    let icon = option.icon ? option.icon : 'fa-solid fa-circle-info';
-    let text = option.discription ? option.discription : 'This is a Info toast.';
-    this.createToast(type, icon, title, text);
-  }
-  loading(title = 'Loading', option = {}) {
-    let type = 'info';
-    let icon = option.icon ? option.icon : 'fa-solid fa-spinner fa-spin';
-    let text = option.discription ? option.discription : 'This is a Loading toast.';
-    this.createToast(type, icon, title, text);
-  }
-}
+    .progressBar.loading {
+    background-color: #5C69F4;
+    }
+
+    .w-5 {
+    width: 20px;
+    }
+
+    .h-5 {
+    height: 20px;
+    }
+
+    .w-3 {
+    width: 12px;
+    }
+
+    .h-3 {
+    height: 12px;
+    }
+    .ml-4{
+    margin-left: 16px;
+    }
+    
+    `;
+
+    document.head.appendChild(style)
+    class Toast {
+      constructor(option = {}) {
+        this.notifications = document.createElement('div');
+        this.notifications.classList.add(option.position ? option.position : 'top-right')
+        this.notifications.setAttribute('style', `--duration:${(option.duration ? option.duration : 5000) / 1000}s`)
+        document.body.appendChild(this.notifications)
+        this.createToast = (type, title, callback = null) => {
+          let newToast = document.createElement('div');
+          newToast.innerHTML = `
+          <div class="toast">
+            <div class="icon-style ${type}">
+              ${icons[type]}
+            </div>
+            <div class="title">${title}</div>
+            ${callback === null ? (option.closeBtn === undefined || option.closeBtn ? `<button type="button" class="close-btn" onclick="(this.parentElement).remove()">${icons["close"]}</button>`:""): ''}
+             ${callback === null ? (option.progress === undefined || option.progress ? `<span class="progressBar ${type}"></span>` : "") : ""}
+          </div>
+          
+          `;
+          this.notifications.appendChild(newToast)
+          if (callback) {
+            callback(newToast)
+          } else {
+            newToast.timeOut = setTimeout(
+              () => newToast.remove(), option.duration ? option.duration : 5000
+            )
+          }
+        }
+      }
+      success(title = 'Success message', option = {}) {
+        let type = 'success';
+        this.createToast(type, title);
+      }
+      error(title = 'Error message', option = {}) {
+        let type = 'error';
+        this.createToast(type, title);
+      }
+      warning(title = 'Warning message', option = {}) {
+        let type = 'warning';
+        this.createToast(type, title);
+      }
+      info(title = 'Info message', option = {}) {
+        let type = 'info';
+        this.createToast(type, title);
+      }
+      danger(title = 'Loading', option = {}) {
+        let type = 'danger';
+        this.createToast(type, title);
+      }
+
+      loading(title = 'Loading', callback = null) {
+        this.createToast('loading', title, callback);
+      }
+    }
